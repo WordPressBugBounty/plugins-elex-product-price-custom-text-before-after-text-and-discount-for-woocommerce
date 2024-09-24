8d3df176a21fc8a6271116d3b38a5f93dd79c7f9
Sean Murphy <sean@iamseanmurphy.com>
2024-09-24 11:01:20 +0000

    4.1.3

---
+++ elex-product-price-custom-text-before-after-text-and-discount-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elula/elex-product-price-custom-text-before-after-text-and-discount-for-woocommerce',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1992f7ab73d14caddbbfa97b634f6e54657e4f32',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elula/elex-product-price-custom-text-before-after-text-and-discount-for-woocommerce' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1992f7ab73d14caddbbfa97b634f6e54657e4f32',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
